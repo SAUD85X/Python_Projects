7b6bd2e56bc4be5b5d488d5eacd470d6170b26cb
Mohammed Saud Ali Siddiqui <saud6445@gmail.com>
2025-09-07 21:12:12 +0530

    Simple number guessing game program

---
+++ number_guess.py
@@ -0,0 +1,20 @@
+import random
+
+print("---Number Guessing Game---")
+print("Guess a Number between 1 and 100 ")
+
+#generate the random number between 1 and 100
+number =random.randint(1,100)
+attempts = 0
+
+while True:
+    guess = int(input("Enter your Guess:"))
+    attempts +=1
+    #check the guess
+    if guess < number:
+        print("Too Low!")
+    elif guess > number:
+        print("Too High!")
+    else:
+        print(f"Congratulations!  You Guessed it in {attempts} attempts ")
+        break
